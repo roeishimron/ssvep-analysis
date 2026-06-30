dd11bba181777a708eef3048967a308c8e1935ce
Roei Shimron <roeishimron@gmail.com.com>
2026-06-30 10:09:23 +0000

    also

--- core_types.py
+++ core_types.py
@@ -8,6 +8,7 @@ TEWF = Tuple[int, int, int, int]
 STEF = Tuple[int, int, int, int]
 TEF = Tuple[int, int, int]
 SEF = Tuple[int, int, int]
+STW = Tuple[int, int, int]
 EF = Tuple[int, int]
 ST = Tuple[int, int]
 STCT = Tuple[int, int, int, int]   # (Subject, Trial, Channel, Time-samples)
@@ -23,7 +24,7 @@ SubjectPower = np.ndarray[EF, np.dtype[np.float64]]           # (E, F)
 RawStudyData = np.ndarray[STCT, np.dtype[np.float64]]         # (S, T, C, Time)
 RawSubjectData = np.ndarray[TCT, np.dtype[np.float64]]        # (T, C, Time) — single subject
 
-ConditionPhases = np.ndarray[ST, np.dtype[np.complex64]]
+ConditionPhases = np.ndarray[STW, np.dtype[np.complex64]]
 
 Array1D_f64 = np.ndarray[Tuple[int], np.dtype[np.float64]]
 Array1D_i64 = np.ndarray[Tuple[int], np.dtype[np.int64]]
